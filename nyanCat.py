74428bf826aa4bcb8ef71072ddc405e09fcac235
Ajda <ajda.vozelj@gmail.com>
2026-03-19 18:38:54 +0100

    skakanje

--- nyanCat.py
+++ nyanCat.py
@@ -5,21 +5,28 @@ pygame.init()
 canvas = pygame.display.set_mode((500,500))
 
 pygame.display.set_caption("Moje okno")
-rdec = pygame.Rect(80,30,15,15)
-
+x = 200
+y = 300
+hitrost =   1
+gravitacija = 0.1
 exit = False
 
 while not exit:
-    pygame.time.wait(4)
+    pygame.time.wait(10)
     canvas.fill((0, 0, 0))
+    hitrost += gravitacija
+    y += hitrost
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:
+        hitrost = -4
+
+
+    kitty = pygame.Rect(x,y,15, 15)
+    pygame.draw.rect(canvas, (150,0,250), kitty)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
-
-    pygame.draw.rect(canvas,barvaR, rdec)
-#------- :)
-    
 
     pygame.display.update()
