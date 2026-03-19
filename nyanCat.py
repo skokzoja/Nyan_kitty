ff3a9f8ee51a2b1e50c2cac3704d1d7741c99e03
Ajda <ajda.vozelj@gmail.com>
2026-03-19 19:23:10 +0100

    platforme

--- nyanCat.py
+++ nyanCat.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 pygame.init()
@@ -5,11 +7,20 @@ pygame.init()
 canvas = pygame.display.set_mode((500,500))
 
 pygame.display.set_caption("Moje okno")
-x = 200
-y = 300
+x = 150
+y = 100
 hitrost =   1
-gravitacija = 0.1
+gravitacija = 0.05
 exit = False
+
+# [ [ sirina,[x, y]], ... ]
+platforme = [ [50,[200,300]], [32, [229, 400]], [65, [350, 150]], [ 30, [500, 200]]]
+
+def novaPlatforma():
+    Nsirina = random.randint(30,90)
+    Nx = 500
+    Ny = random.randint(150,400)
+    platforme.append([Nsirina,[Nx,Ny]])
 
 while not exit:
     pygame.time.wait(10)
@@ -21,6 +32,14 @@ while not exit:
     if keys[pygame.K_SPACE]:
         hitrost = -4
 
+
+    #novaPlatforma()
+    print(platforme)
+
+    for i in platforme:
+        i[1][0] -= 1.5
+        plat =  pygame.Rect(i[1][0],i[1][1], i[0],5 )
+        pygame.draw.rect(canvas, (150, 0, 250), plat)
 
     kitty = pygame.Rect(x,y,15, 15)
     pygame.draw.rect(canvas, (150,0,250), kitty)
